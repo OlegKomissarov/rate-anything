aa37b9dbc3c5fca0e3fcfbb28b8233832a400c16
OlegKomissarov <o.komissarov247@gmail.com>
2023-03-10 23:11:03 +0100

    Start preparing to create panning login background

--- src/components/rate/login/RateStars.tsx
+++ src/components/rate/login/RateStars.tsx
@@ -8,42 +8,19 @@ interface Position {
     y: number;
 }
 
-interface Size {
-    w: number;
-    h: number;
-}
-
-// Todo: Handle case when there are no more space.
-// Todo: Optimize it, when almost no space it will loop too much
 const generateStarPositions = (numStars: number) => {
     const positions: Position[] = [];
 
-    const getCentralPosition = (size: number) => 100 / 2 - size / 2;
-
     const minDistanceBetweenStars = 5;
-    const headerSize: Size = { w: 40, h: 16 };
-    const headerPos: Position = { x: getCentralPosition(headerSize.w), y: 2 };
-    const contentSize: Size = { w: 40, h: 40 };
-    const contentPos: Position = { x: getCentralPosition(contentSize.w), y: 100 / 2 - getCentralPosition(contentSize.h) / 2 };
 
     const isCollidingWithOtherStars = (newPos: Position) =>
         positions.some((pos) => Math.hypot(pos.x - newPos.x, pos.y - newPos.y) < minDistanceBetweenStars);
-
-    const isInsideRestrictedArea = (pos: Position, areaPosition: Position, areaSize: Size) =>
-        pos.x > areaPosition.x &&
-        pos.x < areaPosition.x + areaSize.w &&
-        pos.y > areaPosition.y &&
-        pos.y < areaPosition.y + areaSize.h;
 
     for (let i = 0; i < numStars; i++) {
         let newPos: Position;
         do {
             newPos = { x: getRandomDecimal(10, 90), y: getRandomDecimal(10, 90) };
-        } while (
-            isCollidingWithOtherStars(newPos)
-            || isInsideRestrictedArea(newPos, contentPos, contentSize)
-            || isInsideRestrictedArea(newPos, headerPos, headerSize)
-        );
+        } while (isCollidingWithOtherStars(newPos));
         positions.push(newPos);
     }
 
