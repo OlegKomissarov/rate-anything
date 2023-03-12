a80f782dd72bffe8289a533171ba801cd63ece3d
OlegKomissarov <o.komissarov247@gmail.com>
2023-03-12 15:43:33 +0100

    Move generateStarsBackgroundData to a separate file

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,15 +4,16 @@ import { Rate } from '../components/rate/rateUtils';
 import { signIn } from 'next-auth/react';
 import { validateAverageRateList } from '../components/rate/rateUtils';
 import Image from 'next/image';
-import { generateStarPositions, getClassName, Position } from '../utils';
+import { getClassName, Position } from '../utils';
 import StarsBackground from '../components/rate/login/StarsBackground';
+import generateStarsBackgroundData from '../generateStarsBackgroundData';
 
 const LoginPage = () => {
     const [averageRates, setAverageRates] = useState<Rate[]>([]);
     const [animateAstronaut, setAnimateAstronaut] = useState(false);
 
     const backgroundData = useRef<{ backgroundSize: Position, starPositions: Position[] }>(
-        generateStarPositions(averageRates.length)
+        generateStarsBackgroundData(averageRates.length)
     );
     const { current: { backgroundSize, starPositions } } = backgroundData;
     const [panPos, setPanPos] = useState<Position>({ x: 0, y: 0 });
@@ -22,7 +23,7 @@ const LoginPage = () => {
         const result = await response.json();
         if (response.ok && result && validateAverageRateList(result.averageRateList)) {
             const averageRateList = result.averageRateList;
-            const newBackgroundData = generateStarPositions(averageRateList.length);
+            const newBackgroundData = generateStarsBackgroundData(averageRateList.length);
             backgroundData.current = newBackgroundData;
             setPanPos({
                 x: (window.innerWidth - newBackgroundData.backgroundSize.x) / 2,
