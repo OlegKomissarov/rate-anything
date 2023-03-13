f3b3dfbfc58d2caabed3289fd8d9e6e779d4bde6
OlegKomissarov <o.komissarov247@gmail.com>
2023-03-13 12:04:20 +0100

    Remove unnecessary code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,21 +2,15 @@ import React from 'react';
 import '../styles/main.css';
 import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
-import { useRouter } from 'next/router';
-
-const darkThemePageNames = ['login'];
 
 const App = (props: any) => {
     const { Component, pageProps: { session, ...pageProps } } = props;
 
-    const router = useRouter();
-    const theme = darkThemePageNames.some(pageName => router.pathname.includes(pageName)) ? 'dark' : 'light';
-
     return <SessionProvider session={session}>
-        <div className={`app app--theme--${theme}`}>
-            <Head>
-                <title>Rate Anything</title>
-            </Head>
+        <Head>
+            <title>Rate Anything</title>
+        </Head>
+        <div className="app">
             <Component {...pageProps} />
         </div>
     </SessionProvider>;
