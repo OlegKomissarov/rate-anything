fd24315ff27731416506e72a705c80d3315780ec
OlegKomissarov <o.komissarov247@gmail.com>
2023-03-15 15:06:41 +0100

    Modify viewport meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ const App = (props: any) => {
     return <SessionProvider session={session}>
         <Head>
             <title>Rate Anything</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
         </Head>
         <div className="app">
             <Component {...pageProps} />
