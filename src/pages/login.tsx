e66741f30777ed8eb139b0611996257eee69671b
OlegKomissarov <o.komissarov247@gmail.com>
2023-03-17 12:00:21 +0100

    Revert attempts to fix astronaut animation on ios

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Button from '../components/elements/Button';
 import Header from '../components/layout/Header';
@@ -8,12 +8,6 @@ import { getClassName, isClient } from '../utils/utils';
 
 const LoginPage = () => {
     const [shouldAnimateAstronaut, setShouldAnimateAstronaut] = useState(false);
-    const [isAnimatedAstronaut, setIsAnimatedAstronaut] = useState(false);
-    const [counter, setCounter] = useState(0);
-
-    useEffect(() => {
-        setInterval(() => setCounter(counter => counter + 1), 1000);
-    }, []);
 
     const onClickSignIn = async () => {
         setShouldAnimateAstronaut(true);
@@ -31,30 +25,16 @@ const LoginPage = () => {
         >
             Sign In To Create Your Rate
         </Button>
-        <div style={{ position: 'fixed', left: '25px', top: '75px', color: 'white', zIndex: 99999 }}>
-            {counter}
-        </div>
-        <div style={{ position: 'fixed', left: '25px', top: '25px', color: 'white', zIndex: 99999 }}>
-            {
-                isClient
-                && document.querySelector('.login__astronaut')
-                // @ts-ignore
-                && document.querySelector('.login__astronaut').classList
-                // @ts-ignore
-                && JSON.stringify(document.querySelector('.login__astronaut').classList)        }
-        </div>
         <Image src="/astronaut.webp"
                alt="Astronaut"
                width={200} height={200}
                className={getClassName(
                    'login__astronaut',
-                   shouldAnimateAstronaut && 'login__astronaut--animating',
-                   isAnimatedAstronaut && 'login__astronaut--animated'
+                   shouldAnimateAstronaut && 'login__astronaut--animated'
                )}
                onMouseDown={event => event.stopPropagation()}
                onClick={onClickSignIn}
                priority
-               onAnimationEnd={() => setIsAnimatedAstronaut(true)}
         />
         <StarsBackground otherElements={isClient ? [
             document.querySelector('.login__button')!,
